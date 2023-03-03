bbcf8f0a2daeffc9ba9b43a028a880a8d8d3f474
Devin Raleigh <devinkr@gmail.com>
2023-03-03 10:55:03 -0700

    Add validation for years and exception handling.

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,28 @@ public class Main {
         System.out.println("Hi " + name + ", Thanks for taking the course!");
 
         System.out.println("What year were you born? ");
-        String dateOfBirth = scanner.nextLine();
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+        boolean validDob = false;
+        int age = 0;
+        do {
+            System.out.println("Enter a year of birth >= " +
+                    (currentYear - 125) + " and <= " + currentYear);
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDob = age >= 0;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Characters not allowed. Try again.");
+            }
+        } while(!validDob);
+
         return "So you are " + age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+        if ((dob < minimumYear) || (dob > currentYear)) {
+            return -1;
+        }
+        return currentYear - dob;
     }
 }
